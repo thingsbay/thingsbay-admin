6e230e0ff3771a8d436035853c2a5f14c7410fdd
Unwatch <hjzqhd@qq.com>
2022-05-12 18:07:55 +0800

    [Modify]修改设备列表

--- src/views/device/deviceList/index.vue
+++ src/views/device/deviceList/index.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="p-4">
+  <PageWrapper dense contentFullHeight fixedHeight contentClass="flex">
+    <DeptTree class="w-1/4 xl:w-1/5" @select="handleSelect" />
     <BasicTable
       @register="registerTable"
+      class="w-3/4 xl:w-4/5"
+      :searchInfo="searchInfo"
       :rowSelection="{ type: 'checkbox', selectedRowKeys: checkedKeys, onChange: onSelectChange }"
     >
       <template #toolbar>
@@ -63,19 +66,25 @@
         />
       </template>
     </BasicTable>
-  </div>
+    <AccountModal @register="registerModal" @success="handleSuccess" />
+  </PageWrapper>
 </template>
 <script lang="ts">
-  import { defineComponent, ref } from 'vue';
+  import { defineComponent, ref, reactive } from 'vue';
   import { BasicTable, useTable, TableAction } from '/@/components/Table';
+  import { PageWrapper } from '/@/components/Page';
+  import DeptTree from './DeptTree.vue';
+  import { useModal } from '/@/components/Modal';
   import { columns, searchFormSchema } from './device.data';
   import { demoListApi } from '/@/api/demo/table';
 
   export default defineComponent({
-    components: { BasicTable, TableAction },
+    components: { BasicTable, PageWrapper, DeptTree, TableAction },
     setup() {
       const checkedKeys = ref<Array<string | number>>([]);
-      const [registerTable] = useTable({
+      const [registerModal, { openModal }] = useModal();
+      const searchInfo = reactive<Recordable>({});
+      const [registerTable, { reload, updateTableDataRecord }] = useTable({
         // 表格标题
         title: '设备列表',
         // 请求接口
@@ -124,12 +133,38 @@
       function deleteDevice() {
         // todo
       }
+
+      function handleCreate() {
+        openModal(true, {
+          isUpdate: false,
+        });
+      }
+
       function handleEdit(record: Recordable) {
         console.log('点击了编辑', record);
+        console.log(record);
+        openModal(true, {
+          record,
+          isUpdate: true,
+        });
       }
+
       function handleDelete(record: Recordable) {
         console.log('点击了删除', record);
+        console.log(record);
       }
+
+      function handleSuccess({ isUpdate, values }) {
+        if (isUpdate) {
+          // 演示不刷新表格直接更新内部数据。
+          // 注意：updateTableDataRecord要求表格的rowKey属性为string并且存在于每一行的record的keys中
+          const result = updateTableDataRecord(values.id, values);
+          console.log(result);
+        } else {
+          reload();
+        }
+      }
+
       function handleOpen(record: Recordable) {
         console.log('点击了启用', record);
       }
@@ -139,15 +174,25 @@
         checkedKeys.value = selectedRowKeys;
       }
 
+      function handleSelect(deptId = '') {
+        searchInfo.deptId = deptId;
+        reload();
+      }
+
       return {
         registerTable,
+        registerModal,
         saveDevice,
         deleteDevice,
+        handleCreate,
         handleEdit,
         handleDelete,
         handleOpen,
+        handleSuccess,
         checkedKeys,
         onSelectChange,
+        handleSelect,
+        searchInfo,
       };
     },
   });
